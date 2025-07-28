1d95abe14df3242863b74690da08bc92cb0dd97d
Walter Diaz <walter.diaz@qualifiedigital.com>
2025-07-27 23:18:40 -0600

    integrate Cypress Cloud for test recording and insight

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: 'vzgbna', 
+  pageLoadTimeout: 600000,
+  defaultCommandTimeout: 60000,
+  video: true,
+  screenshotOnRunFailure: true,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
-});
+}) 
